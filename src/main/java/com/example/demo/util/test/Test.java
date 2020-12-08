f879c9cda3ed63c851df01a43b1d959958970985
xueyangsheng <xueyangsheng@9fbank.com.cn>
2020-12-08 18:02:09 +0800

    demo项目上传: 项目结构修改 20201208

--- src/main/java/com/example/demo/util/test/Test.java
+++ src/main/java/com/example/demo/util/test/Test.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.example.demo.util.test;
 
 
 import java.text.ParseException;
--- src/main/java/com/example/demo/util/test/ThreadPools.java
+++ src/main/java/com/example/demo/util/test/ThreadPools.java
@@ -1,4 +1,4 @@
-package com.example.demo.util;
+package com.example.demo.util.test;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
